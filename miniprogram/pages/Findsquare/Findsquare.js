d5706214a390a2ad11f20148450bc4e6e59006d2
jason666ke <a773449752@163.com>
2021-01-07 22:46:22 +0800

    Initial Commit

---
+++ miniprogram/pages/Findsquare/Findsquare.js
@@ -0,0 +1,89 @@
+// pages/Findsquare/Findsquare.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+      {
+        thingsImage:'../../images/camera.png',
+        thingsName:'笔记本',
+        lostTime:'2019年4月2日13：24',
+        thingDescription:'白色封皮，里面是英语笔记',
+        buttomtext:'查看',
+      },
+      {
+        thingsImage:'../../images/add.png',
+        thingsName:'手表',
+        lostTime:'2021年1月7日13：24',
+        thingDescription:'红色表带，皮质……',
+        buttomtext:'查看',
+      },
+      {
+        thingsImage:'../../images/house.png',
+        thingsName:'手链',
+        lostTime:'2021年1月8日13：04',
+        thingDescription:'ifaodaiojoifiofjsd',
+        buttomtext:'查看',
+      }
+    ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
